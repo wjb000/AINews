837e549f38bee7ae3b3f2f637e5e954f070b0331
net1ife <127271713+net1ife@users.noreply.github.com>
2023-08-31 16:27:34 -0500

    Update AINews.py

--- AINews.py
+++ AINews.py
@@ -1,18 +1,23 @@
+import os
 import requests
 from newspaper import Article
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 def fetch_news(stock):
-    try:
-        url = f"https://newsapi.org/v2/everything?q={stock}&apiKey=YOUR_API_KEY"
-        response = requests.get(url)
-        response.raise_for_status()  # Raise HTTPError for bad responses
-        data = response.json()
-        return [(article['title'], article['url']) for article in data.get('articles', [])]
-    except requests.RequestException as e:
-        print(f"Failed to fetch news: {e}")
+    api_key = "your_actual_api_key_here"  # Hardcode the API key
+    if not api_key:
+        print("No API Key found.")
         return []
-
+    
+    url = f"https://newsapi.org/v2/everything?q={stock}&apiKey={api_key}"
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print(f"Failed to fetch news. Status code: {response.status_code}")
+        return []
+    
+    data = response.json()
+    return [(article['title'], article['url']) for article in data.get('articles', [])]
 
 def extract_text(url):
     article = Article(url)
@@ -24,39 +29,28 @@ def summarize_text(text):
     model_name = "facebook/bart-large-cnn"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    
+
     inputs = tokenizer([text], max_length=1024, truncation=True, return_tensors='pt')
     summary_ids = model.generate(inputs.input_ids, num_beams=4, min_length=30, max_length=100, early_stopping=True)
     
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-def get_stock_news_summary(stock):
+def main():
+    stock = input("Enter the stock you're interested in: ")
     news_data = fetch_news(stock)
-    summaries = []
     
     if not news_data:
-        return []
+        print("No news found for the specified stock.")
+        return
     
-    for title, url in news_data:
+    for i, (title, url) in enumerate(news_data):
+        print(f"\nArticle {i + 1}: {title}")
+        print(f"URL: {url}")
+        
         text = extract_text(url)
         summary = summarize_text(text)
         
-        summaries.append({
-            "title": title,
-            "url": url,
-            "summary": summary
-        })
-        
-    return summaries
+        print(f"Summary: {summary}")
 
 if __name__ == "__main__":
-    stock = input("Enter the stock you're interested in: ")
-    summaries = get_stock_news_summary(stock)
-    
-    if len(summaries) == 0:
-        print("No news found for the specified stock.")
-    else:
-        for i, summary in enumerate(summaries):
-            print(f"\nArticle {i+1}: {summary['title']}")
-            print(f"URL: {summary['url']}")
-            print(f"Summary: {summary['summary']}")
+    main()
